c4a2bffd49e200740758886cedf9c3c27cc3a19a
NahuelPardo89 <nahuel.pardo74@gmail.com>
2024-04-25 15:30:23 -0300

    refresh token y checktoken done

--- PassKeeperWeb/frontend/PassKeeper/src/app/auth/auth.service.ts
+++ PassKeeperWeb/frontend/PassKeeper/src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { LoginCredentials, LoginResponse } from './auth.models';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient, HttpResponse} from '@angular/common/http';
+import { JwtTokens, LoginCredentials, LoginResponse } from './auth.models';
+import { BehaviorSubject, Observable, tap, throwError } from 'rxjs';
 import {enviroment} from '../../../enviroment/enviroment';
+import { StoreService } from './store.service';
 
 
 
@@ -11,24 +12,33 @@ import {enviroment} from '../../../enviroment/enviroment';
 })
 export class AuthService {
   loginUrl=enviroment.api_Url +'account/login/';
+  refreshTokenUrl=enviroment.api_Url+'account/refresh/';
   
   private islogged = new BehaviorSubject<boolean>(false); 
 
-  constructor(private http: HttpClient,) {
-    if (this.checkToken()) {
-      this.islogged.next(true);
-     
-    } else {
-      this.islogged.next(false);
-    }
-   }
+  constructor(private http: HttpClient, private storeService: StoreService) {
+    this.checkToken()
+  }
   
   logIn(credentials:LoginCredentials):Observable<LoginResponse> {
     return  this.http.post<LoginResponse>(this.loginUrl, credentials)
   }
 
-  logOut(){}
+  logOut(){
+    this.storeService.clearLocalStorage()
+  }
+  
+  refreshToken(): Observable<HttpResponse<JwtTokens>> {
+    const refreshToken = {"refresh":this.storeService.getRefreshToken()};
+    console.log(refreshToken)
+    if (!refreshToken) {
+      this.logOut();
+      return throwError(() => new Error('Refresh token no disponible'));
+    }
 
+    return this.http.post<HttpResponse<JwtTokens>>(this.refreshTokenUrl,refreshToken)
+      
+  }
   setIsLogged(islogged:boolean){
     this.islogged.next(islogged);
   }
@@ -37,7 +47,36 @@ export class AuthService {
     return this.islogged.asObservable();
   }
   
-  checkToken():boolean {
-    return true
+  checkToken() {
+    this.refreshToken().subscribe({
+      next:(response:any) => {
+        console.log(response.access)
+        const access=(response.access)
+        const refresh=response.refresh
+        if (access && refresh ) {
+          this.storeService.setAccessToken(access)
+          this.storeService.setRefreshToken(refresh)
+          this.islogged.next(true)
+          console.log('refresh successful');
+          
+        } else {
+          this.logOut()
+          // Maneja el caso donde no hay tokens en la respuesta.
+          console.error('Invalid credentials', response);
+        }
+      },
+      error: (err) => {
+        this.logOut()
+        let errorMessage = 'An error occurred during login.';
+        if (err.status === 400) {
+          // Puedes obtener el mensaje de error de la respuesta del backend aquí
+          errorMessage = err.error.message || 'Credentials are incorrect.';
+        }
+        console.error(errorMessage);
+        // Aquí podrías mostrar el mensaje de error en la interfaz de usuario
+        
+      }
+    })
+   
   }
 }
