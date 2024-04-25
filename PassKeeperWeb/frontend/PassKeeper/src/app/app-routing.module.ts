99931a52da5d06421933eb53fc07d26ccb914d1c
NahuelPardo89 <nahuel.pardo74@gmail.com>
2024-04-25 10:41:45 -0300

    agregado a modulo home componente main y header

--- PassKeeperWeb/frontend/PassKeeper/src/app/app-routing.module.ts
+++ PassKeeperWeb/frontend/PassKeeper/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
 
 
 const routes: Routes = [
+  {path: '', component:HomeComponent},
   {path: 'auth',loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
 ];
 
--- PassKeeperWeb/frontend/PassKeeper/src/app/home/home.module.ts
+++ PassKeeperWeb/frontend/PassKeeper/src/app/home/home.module.ts
@@ -5,13 +5,17 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     AboutUsComponent,
-    ContactComponent
+    ContactComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     CommonModule,
