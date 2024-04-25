e1a53c4c136d3b4d87c6f22b962c3cdad8bc44e3
NahuelPardo89 <nahuel.pardo74@gmail.com>
2024-04-25 13:32:21 -0300

    routh admin

--- PassKeeperWeb/frontend/PassKeeper/src/app/app-routing.module.ts
+++ PassKeeperWeb/frontend/PassKeeper/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home/home.component';
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'auth',loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: 'admin',loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
 ];
 
 @NgModule({
