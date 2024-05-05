f5338a5bd861f31e5400db7c2af2c701506684af
NahuelPardo89 <nahuel.pardo74@gmail.com>
2024-05-05 10:03:44 -0300

    creacion app payments

--- API/core/urls.py
+++ API/core/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('account/', include('apps.users.api.urls')),
+    path('payments/', include('apps.payments.api.urls')),
 ]
