3088d3ee11021612fd96938499d9566e401fb92e
Joana <106889601+JoanaGM44@users.noreply.github.com>
2024-05-13 17:10:40 -0300

    se agrega servicio de contacto

--- WEB/src/app/home/contact.service.ts
+++ WEB/src/app/home/contact.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  private apiUrl = "http://localhost:8000/API/";
+
+  constructor(private http: HttpClient) { }
+
+  sendMessage(mensaje: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, mensaje);
+  }
+
 }
--- WEB/src/app/home/contact/contact.component.ts
+++ WEB/src/app/home/contact/contact.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+  public formContacto!: FormGroup;
+
+  constructor( private fb:FormBuilder, private contactService: ContactService){}
+
+  ngOnInit(): void {
+    this.formContacto=this.fb.group({
+
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', Validators.required]
+    });  
+  }
+
+    sendMessage(){
+
+      if (this.formContacto.valid) {
+        this.contactService.sendMessage(this.formContacto.value).subscribe(
+          respuesta => {
+            console.log('Mensaje enviado:', respuesta);
+            
+          },
+          error => {
+            console.error('Error al enviar mensaje:', error);
+            
+          }
+        );
+      }
+    }
+
 
 }
