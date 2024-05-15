b42131ff42aa9bbf82b25b607a1de9e35c083f4f
Vale L <106398144+vlysi@users.noreply.github.com>
2024-05-14 21:05:00 -0300

    Refactorización

--- WEB/src/app/home/home.module.ts
+++ WEB/src/app/home/home.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
+
 
 
 @NgModule({
   declarations: [
-    HomeComponent,
     AboutUsComponent,
     ContactComponent,
     HeaderComponent,
