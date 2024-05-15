d133d1247593a639f547115b48587770a444a2e7
Joana <106889601+JoanaGM44@users.noreply.github.com>
2024-05-15 11:10:42 -0300

    se agrega apiUrl

--- WEB/src/app/home/contact.service.ts
+++ WEB/src/app/home/contact.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  private apiUrl = "http://localhost:8000/API/";
+  contactUrl = environment.apiUrl +'account/contact/';
 
   constructor(private http: HttpClient) { }
 
   sendMessage(mensaje: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, mensaje);
+    return this.http.post<any>(this.contactUrl, mensaje);
   }
 
 }
