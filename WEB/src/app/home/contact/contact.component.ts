f29b7285c70fc617c5f7653d95aed0328e5e7316
Joana <106889601+JoanaGM44@users.noreply.github.com>
2024-05-16 00:13:27 -0300

    mensaje de exito

--- WEB/src/app/home/contact/contact.component.ts
+++ WEB/src/app/home/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ContactService } from '../contact.service';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -9,16 +9,23 @@ import { ContactService } from '../contact.service';
 })
 export class ContactComponent implements OnInit{
   public formContacto!: FormGroup;
+  mensaje:string = '';
+  error:string = '';
+  name:string='';
+  lastname:string='';
+  mail:string='';
+  msg:string='';
+
 
   constructor( private fb:FormBuilder, private contactService: ContactService){}
 
   ngOnInit(): void {
     this.formContacto=this.fb.group({
 
-      nombre: ['', [Validators.required, Validators.minLength(3)]],
-      apellido: ['',[ Validators.required, Validators.minLength(3) ]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      last_name: ['',[ Validators.required, Validators.minLength(3) ]],
       email: ['', [Validators.required, Validators.email]],
-      mensaje: ['', Validators.required ]
+      message: ['', Validators.required ]
     });  
   }
 
@@ -28,10 +35,17 @@ export class ContactComponent implements OnInit{
         this.contactService.sendMessage(this.formContacto.value).subscribe(
           respuesta => {
             console.log('Mensaje enviado:', respuesta);
+            this.mensaje='Mensaje enviado correctamente';
+            this.name='';
+            this.lastname='';
+            this.mail='';
+            this.msg='';
+
             
           },
           error => {
             console.error('Error al enviar mensaje:', error);
+            this.error='Error al enviar el mensaje ' , error;
             
           }
         );
