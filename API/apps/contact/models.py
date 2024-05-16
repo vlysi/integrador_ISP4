8f73a83580172d7e6f735b957be8f1e411fde78c
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2024-05-16 00:52:30 -0300

    Admin Contact
    Se agrega la administracion de mensajes de contactos

--- API/apps/contact/models.py
+++ API/apps/contact/models.py
@@ -6,6 +6,10 @@ class Message(models.Model):
     email=models.EmailField(blank=False, null=False)
     message = models.TextField()  
     
+    class Meta:
+        verbose_name = 'Mensaje'
+        verbose_name_plural = 'Mensajes'
+    
     def __str__(self):
         return f"mensaje de: {self.name} {self.last_name} - {self.email}- {self.message} "
 
