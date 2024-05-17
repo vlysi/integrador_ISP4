86d4dc08b9ec0b22cef6226fcf18bbf189ce36e1
Catriel <cartup90@gmail.com>
2024-05-17 11:38:07 -0300

    listado de Mensajes funcionando

--- WEB/src/app/admin/table-message/table-message.component.ts
+++ WEB/src/app/admin/table-message/table-message.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-table-message',
   templateUrl: './table-message.component.html',
   styleUrls: ['./table-message.component.css']
 })
-export class TableMessageComponent {
+export class TableMessageComponent implements OnInit {
 
+  messages: any[] = [];
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.getMessages();
+    
+  }
+
+  getMessages(): void {
+    this.contactService.getMessages().subscribe(
+      (data: any[]) => {
+        this.messages = data;
+      },
+      (error:any) => {
+        console.error('Error al obtener los mensajes:', error);
+      }
+      
+    );
+    
+  }
+  
 }
